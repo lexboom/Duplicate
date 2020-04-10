052fcd792ffbccdeb58766c5173336bc9dfe3886
lexboom <47512002+lexboom@users.noreply.github.com>
2020-04-10 11:45:44 -0400

    Update DuplicateFinder.py

--- DuplicateFinder.py
+++ DuplicateFinder.py
@@ -1,4 +1,3 @@
-
 import os
 import sys
 import hashlib
@@ -12,3 +11,27 @@ def hashfile(path, blocksize = 65536):
         buf = afile.read(blocksize)
     afile.close()
     return hasher.hexdigest()
+
+    def findDup(parentFolder):
+    # Dups in format {hash:[names]}
+    dups = {}
+    for dirName, subdirs, fileList in os.walk(parentFolder):
+        print('Scanning %s...' % dirName)
+        for filename in fileList:
+            # Get the path to the file
+            path = os.path.join(dirName, filename)
+            # Calculate hash
+            file_hash = hashfile(path)
+            # Add or append the file path
+            if file_hash in dups:
+                dups[file_hash].append(path)
+            else:
+                dups[file_hash] = [path]
+    return dups
+# Joins two dictionaries
+def joinDicts(dict1, dict2):
+    for key in dict2.keys():
+        if key in dict1:
+            dict1[key] = dict1[key] + dict2[key]
+        else:
+            dict1[key] = dict2[key]
